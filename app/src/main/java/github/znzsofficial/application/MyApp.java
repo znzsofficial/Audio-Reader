bad96b3a278f50956618b9c18994739bd53281d4
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2023-02-14 14:34:31 +0800

    Add files via upload

--- app/src/main/java/github/znzsofficial/application/MyApp.java
+++ app/src/main/java/github/znzsofficial/application/MyApp.java
@@ -1,1 +1,14 @@
+package github.znzsofficial.application;
 
+import android.app.Application;
+import com.google.android.material.color.DynamicColors;
+import com.google.android.material.snackbar.Snackbar;
+import github.znzsofficial.activity.MainActivity;
+
+public class MyApp extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    DynamicColors.applyToActivitiesIfAvailable(this);
+  }
+}
